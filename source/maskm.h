21ce1da55862a9025386acecf3a4e1381c9ce519
Samuele Capacci <samuele_capacci@outlook.it>
2023-01-16 17:35:32 +0100

    Create maskm.h

---
+++ source/maskm.h
@@ -0,0 +1,22 @@
+#ifndef MASKM
+#define MASKM
+#include "main.h"
+#include "docm.h"
+
+typedef struct{
+    wchar_t** matrix;
+    int posx;
+    int posy;
+    int rows;
+    int cols;
+    int cursor_x;
+    int cursor_y;
+} Guiw_mask;
+
+Guiw_mask* alloc_guiw_mask(int rows, int cols);
+void dealloc_guiw_mask(Guiw_mask* mask);
+int populate_guiw_mask(Guiw_mask* mask, Document* doc);
+void move_guiw_mask(Guiw_mask* mask, int deltax, int deltay);
+void update_mask(Guiw_mask* mask, Document* doc);
+
+#endif //MASKM
